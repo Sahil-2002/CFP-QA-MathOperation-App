ad2af16b915f06a7433c86e9b52686c04baa24ee
Sahil-2002 <sahila.khandekar@gmail.com>
2024-02-20 18:48:05 +0530

    performing math operations using lambda

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.function.Function;
+
+@FunctionalInterface
+interface Mathfunction {
+    public int calculate(int a, int b);
+
+}
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Math operation App");
+        System.out.println(" ");
+        Mathfunction add = (a, b) -> a + b;
+        System.out.println("Addition is :"+add.calculate(20, 30));
+        Mathfunction sub = (a, b) -> a - b;
+        System.out.println("Subtraction is "+sub.calculate(20, 30));
+        Mathfunction div = (a, b) -> a / b;
+        System.out.println("Division is "+div.calculate(20, 30));
+        Mathfunction mul =(a,b)-> a*b;
+        System.out.println("Multiplication is "+mul.calculate(20,30));
     }
 }
